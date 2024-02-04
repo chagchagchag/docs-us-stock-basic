aa944d236bf68f2ec1e94f755c1d03dd28f3ff31
chagchagchag <chagchagchag.dev@gmail.com>
2024-02-04 13:43:32 +0900

    seeking alpha

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/my-nextra-template',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/my-nextra-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
